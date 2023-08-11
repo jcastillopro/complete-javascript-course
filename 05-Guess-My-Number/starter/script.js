8ad449ca09231cc20125b4cb9243231b00e6d297
jcastillopro <jonathan@jcastillo.pro>
2023-08-11 12:09:48 -0500

    Práctica de eventos

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+let input = document.querySelector(".guess").value
+
+if (!input){
+    document.querySelector(".check").addEventListener("click", function(){
+        
+        document.querySelector(".message").textContent = "⛔ No number!"
+    })
+}
