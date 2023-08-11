28dd1c94f20e2325225ed9340796eda52060ef4c
jcastillopro <jonathan@jcastillo.pro>
2023-08-11 12:06:23 -0500

    Revert "practica de eventos"
    This reverts commit b7e102301071e73c73fd536678cf0f77be35a81b.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,1 @@
 'use strict';
-let input = document.querySelector(".guess").value
-
-if (!input){
-    document.querySelector(".check").addEventListener("click", function(){
-        
-        document.querySelector(".message").textContent = "⛔ No number!"
-    })
-}
